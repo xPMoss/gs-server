e8316aa510316e484ada103df1822eae939d6c84
patrikmo <patrik.mossberg@av.se>
2024-03-22 00:14:24 +0100

    init

---
+++ server.js
@@ -0,0 +1,27 @@
+// This is your test secret API key.
+const stripe = require('stripe')('sk_test_51Owtf1065nngbryAiEp1d2oa0VifQpOyKngvHNREZIoTEM11oPaJH5tnvwZFNp4Euqn583KqwmOAMKXAjxgsNFBa00p0w2eNsq');
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+const YOUR_DOMAIN = 'http://localhost:4242';
+
+app.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: '{{PRICE_ID}}',
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}/success.html`,
+    cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+    automatic_tax: {enabled: true},
+  });
+
+  res.redirect(303, session.url);
+});
+
+app.listen(4242, () => console.log('Running on port 4242'));
